271e06ae06c4f0cc6a9d7777e0666fa10b66874e
Mark Whitaker <mark@bitrot.net>
2026-05-30 22:25:20 +0100

    Tidy up excerpt code

--- src/utils/excerpt.ts
+++ src/utils/excerpt.ts
@@ -1,10 +1,12 @@
 export function getExcerpt(body: string): string {
   const text = body
+    .replace(/<br\s?\/>/g, ' ')
     .replace(/!\[.*?\]\(.*?\)/g, '')
     .replace(/\[([^\]]+)\]\([^)]+\)/g, '$1')
     .replace(/^#{1,6}\s+/gm, '')
     .replace(/[*`>_~]/g, '')
     .replace(/\s+/g, ' ')
+    .replace(/\\/g, '')
     .trim();
   if (text.length <= 150) return text;
   const truncated = text.slice(0, 150);
