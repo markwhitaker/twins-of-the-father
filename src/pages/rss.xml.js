b3bd3de4c11154faa95cb1a1c56ca71697371aad
Mark Whitaker <mark@bitrot.net>
2026-05-30 21:48:06 +0100

    Update title

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -6,7 +6,7 @@ export async function GET(context) {
   const posts = await getCollection('posts', ({ data }) => !data.draft);
   const sorted = posts.sort((a, b) => b.data.date.valueOf() - a.data.date.valueOf());
   return rss({
-    title: 'Twins of the Father',
+    title: 'The Twins of the Father',
     description: "A father's blog about raising twins.",
     site: context.site,
     items: sorted.map(post => ({
