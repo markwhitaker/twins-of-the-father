d2958519ea8c20d668fd0eef7cf945fb3638f3a1
Mark Whitaker <mark@bitrot.net>
2026-05-30 12:22:23 +0100

    feat: add previous/next post navigation to post footer

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,14 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+declare module '@pagefind/default-ui' {
+  interface PagefindUIOptions {
+    element: string;
+    showSubResults?: boolean;
+    autofocus?: boolean;
+  }
+  export class PagefindUI {
+    constructor(options: PagefindUIOptions);
+    triggerSearch(query: string): void;
+  }
+}
